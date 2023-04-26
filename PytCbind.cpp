849eab1e2f68dff22f3f3d6a239b256178097160
Adwit Creationz <adwait.34@gmail.com>
2023-04-26 22:32:32 +0530

    Changes lol

--- PytCbind.cpp
+++ PytCbind.cpp
@@ -1,14 +1,13 @@
 // PytCbind.cpp : This file contains the 'main' function. Program execution begins and ends there.
-
+#include <string>
 #include <iostream>
 #include <fstream>
 
 int main(int argc , char* argv[])
 {
-    if (argc == 1) {
-        return 0;
-    }
-    else {
-        
-    }
+    std::string path;
+    std::cout << "Enter Address of file: ";
+    std::getline(std::cin, path);
+    std::cout << "Loading File at : " << path << std::endl;
+    return 0;
 }
