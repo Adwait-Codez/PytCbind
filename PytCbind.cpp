717436dd3507afc1d9ff7769314beb613f8992b1
Adwait Creationz <adwait.34@gmail.com>
2023-04-28 21:26:33 +0530

    File Loading Test

--- PytCbind.cpp
+++ PytCbind.cpp
@@ -6,8 +6,14 @@
 int main(int argc , char* argv[])
 {
     std::string path;
+    std::string Data;
     std::cout << "Enter Address of file: ";
     std::getline(std::cin, path);
     std::cout << "Loading File at : " << path << std::endl;
+    //loding starts here.
+    std::ifstream in(path);
+    in >> Data;
+    std::cout << Data;
+    in.close();
     return 0;
 }
